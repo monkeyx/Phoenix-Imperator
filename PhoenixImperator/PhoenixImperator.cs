4e0d93363a15dfa5fe4d0d24c1de61742b95f95c
Seyed Razavi <monkeyx@gmail.com>
2015-10-12 23:06:51 +0100

    Basic user login save

--- PhoenixImperator/PhoenixImperator.cs
+++ PhoenixImperator/PhoenixImperator.cs
@@ -1,27 +1,50 @@
 ﻿using System;
+using System.IO;
 
 using Xamarin.Forms;
 
+using Phoenix.BL.Entities;
+using Phoenix.BL.Managers;
 using Phoenix.DL;
 
 namespace PhoenixImperator
 {
 	public class App : Application, IDatabase
 	{
+		public static NavigationPage NavigationPage { get; set; }
+
+		public static User User { get; set; }
+
+		public static UserManager UserManager { get; set; }
+
 		public App ()
 		{
-			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
+			var sqliteFilename = "Phoenix.db3";
+			string documentsPath = Environment.GetFolderPath (Environment.SpecialFolder.Personal); // Documents folder
+			string libraryPath = Path.Combine (documentsPath, "..", "Library"); // Library folder
+			var path = Path.Combine(libraryPath, sqliteFilename);
+			_dbConnection = new SQLite.SQLiteConnection (path);
+
+			PhoenixDatabase.DatabaseProvider = this;
+			PhoenixDatabase.CreateTables ();
+
+			App.UserManager = new UserManager ();
+			int userCount = App.UserManager.Count ();
+			Console.WriteLine ("Users: " + userCount);
+			UserForm userForm = new UserForm();
+			MainPage = new NavigationPage(userForm);
+			if (userCount > 0) {
+				App.UserManager.First ((user) => {
+					// The root page of your application
+					userForm.UserCode = user.Code;
+					userForm.UserId = user.Id;
+				});
+			}
+		}
+
+		public SQLite.SQLiteConnection GetConnection()
+		{
+			return _dbConnection;
 		}
 
 		protected override void OnStart ()
@@ -38,6 +61,8 @@ namespace PhoenixImperator
 		{
 			// Handle when your app resumes
 		}
+
+		private SQLite.SQLiteConnection _dbConnection;
 	}
 }
 
